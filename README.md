01e91f8c1f060ad4e7602b9a7d5b09cc6198704a
Connor Bulakites <connor@bulakites.net>
2018-03-26 22:30:07 -0400

    Follow jsmidgen and synth-js examples to generate a simple scale

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Song Bird requires Node.JS version 9.9.0 or higher.
+
+Run 'npm start' in the root directory to produce an example WAV file.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-process.stdout.write('Hello, world!\n');
+const fs = require('fs');
+const Midi = require('jsmidgen');
+
+const file = new Midi.File();
+const track = new Midi.Track();
+file.addTrack(track);
+track.addNote(0, 'c4', 64);
+track.addNote(0, 'd4', 64);
+track.addNote(0, 'e4', 64);
+track.addNote(0, 'f4', 64);
+track.addNote(0, 'g4', 64);
+track.addNote(0, 'a4', 64);
+track.addNote(0, 'b4', 64);
+track.addNote(0, 'c5', 64);
+
+fs.writeFileSync('out/test.mid', file.toBytes(), 'binary');
+
+const synth = require('synth-js');
+const midiBuffer = fs.readFileSync('out/test.mid');
+const waveBuffer = synth.midiToWav(midiBuffer).toBuffer();
+
+fs.writeFileSync('out/test.wav', waveBuffer, 'binary');
