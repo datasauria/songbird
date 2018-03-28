7e0fa14e9f2676d8f37a36c584665334de2a7a1f
Connor Bulakites <connor@bulakites.net>
2018-03-27 23:59:14 -0400

    Add configuration options

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 const fs = require('fs');
 const Midi = require('jsmidgen');
-const { exec } = require('child_process');
-const { soundfont } = require('../config'); 
+const { execSync } = require('child_process');
+const { font, targets } = require('../config'); 
 
-const file = new Midi.File();
-const track = new Midi.Track();
+process.stdout.write('Generating WAV files...\n');
 
-file.addTrack(track);
+targets.forEach(({ instrument, note, length }) => {
+  const file = new Midi.File();
+  const track = new Midi.Track();
 
-// Set instrument to strings:
-track.setInstrument(0, 42);
+  file.addTrack(track);
 
-// Add notes to the track:
-track.addNote(0, 'c4', 64);
-track.addNote(0, 'd4', 64);
-track.addNote(0, 'e4', 64);
-track.addNote(0, 'f4', 64);
-track.addNote(0, 'g4', 64);
-track.addNote(0, 'a4', 64);
-track.addNote(0, 'b4', 64);
-track.addNote(0, 'c5', 64);
+  // Set instrument to strings:
+  track.setInstrument(0, instrument);
 
-// Create the MIDI file:
-fs.writeFileSync('out/test.mid', file.toBytes(), 'binary');
+  // Add note to the track:
+  track.addNote(0, note, length);
 
-// Use fluidsynth and general user font to synthesize WAV file:
-exec(`fluidsynth -F out/test.wav ${soundfont} out/test.mid`);
+  // Create the MIDI file:
+  fs.writeFileSync(`out/${instrument}_${note}.mid`, file.toBytes(), 'binary');
+
+  // Use fluidsynth and general user font to synthesize WAV file:
+  execSync(`fluidsynth -F out/${instrument}_${note}.wav ${font} out/${instrument}_${note}.mid`);
+
+  // Remove the MIDI file:
+  execSync(`rm out/${instrument}_${note}.mid`);
+});
+
+process.stdout.write('Done! Check the "out" directory.\n');
