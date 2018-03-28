28746bb928b6d4dd8b5c35991e67a6c0026ff08f
Connor Bulakites <connor@bulakites.net>
2018-03-27 22:29:25 -0400

    Remove synth-js and update README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Song Bird requires Node.JS version 9.9.0 or higher.
+SongBird requires Node.JS version 9.9.0 or higher.
+
+SongBird requires fluidsynth to be installed. On MacOS, this can be done
+using the command "brew install fluid-synth --with-libsndfile". For other
+systems, please consult the documentation at http://www.fluidsynth.org.
 
 Run 'npm start' in the root directory to produce an example WAV file.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 const Midi = require('jsmidgen');
+const { exec } = require('child_process');
+
 const file = new Midi.File();
 const track = new Midi.Track();
-file.addTrack(track);
-track.setInstrument(0, 42);
-track.addNote(0, 'c4', 256);
 
+file.addTrack(track);
+
+// Set instrument to strings:
+track.setInstrument(0, 42);
+
+// Add notes to the track:
+track.addNote(0, 'c4', 64);
+track.addNote(0, 'd4', 64);
+track.addNote(0, 'e4', 64);
+track.addNote(0, 'f4', 64);
+track.addNote(0, 'g4', 64);
+track.addNote(0, 'a5', 64);
+track.addNote(0, 'b5', 64);
+track.addNote(0, 'c5', 64);
+
+// Create the MIDI file:
 fs.writeFileSync('out/test.mid', file.toBytes(), 'binary');
 
-const synth = require('synth-js');
-const midiBuffer = fs.readFileSync('out/test.mid');
-const waveBuffer = synth.midiToWav(midiBuffer).toBuffer();
+// Use fluidsynth and general user font to synthesize WAV file:
+exec("fluidsynth -F out/test.wav soundfonts/general_user_gs/general_user_gs.sf2 out/test.mid");
 
-fs.writeFileSync('out/test.wav', waveBuffer, 'binary');
+// Delete the MIDI file:
+exec("rm out/test.mid");
