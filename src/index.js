c10980d2c088777519e589df0fca2c67fb29eb04
Connor Bulakites <connor@bulakites.net>
2018-03-27 22:08:03 -0400

    Add General User SoundFont
    Preparing to replace synth-js with fluidsynth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 const fs = require('fs');
 const Midi = require('jsmidgen');
-
 const file = new Midi.File();
 const track = new Midi.Track();
 file.addTrack(track);
-track.addNote(0, 'c4', 64);
-track.addNote(0, 'd4', 64);
-track.addNote(0, 'e4', 64);
-track.addNote(0, 'f4', 64);
-track.addNote(0, 'g4', 64);
-track.addNote(0, 'a4', 64);
-track.addNote(0, 'b4', 64);
-track.addNote(0, 'c5', 64);
+track.setInstrument(0, 42);
+track.addNote(0, 'c4', 256);
 
 fs.writeFileSync('out/test.mid', file.toBytes(), 'binary');
 
